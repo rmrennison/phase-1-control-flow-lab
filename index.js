a080af53626ef7f67839ef6b8546c3f9ffc23635
RyanRennison <rmrennison@gmail.com>
2021-06-28 13:36:16 -0400

    commit tree 64136ecd3ba50f1e7bc3bc863c3905c2d151392e

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride = 300){
+  
+    if (ride <= 400){
+      return 'This one is on me!';
+  } else if (ride = 600){
+      return 'I will gladly take your thirty bucks.'
+  }
 }
 
 function ternaryCheckCity(){
